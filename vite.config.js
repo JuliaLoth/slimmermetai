a0a303a9715f3739a4a66811630515f2220d780b
Julia Loth <julia@loth.nl>
2025-06-06 12:06:45 +0200

    Fix: Vite ESM support, Symfony CLI security checker, lint-staged config, auto PHPCBF

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 import purgecss from 'vite-plugin-purgecss';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => ({
   plugins: [
