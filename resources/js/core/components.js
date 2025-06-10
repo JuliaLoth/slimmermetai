0e0c0805528cfcc6d33986d07724c381e20c4178
Julia Loth <julia@loth.nl>
2025-06-10 15:01:34 +0200

    Fix GitHub Actions Issues: Security scan composer audit + CSS duplicate imports resolved

--- resources/js/core/components.js
+++ resources/js/core/components.js
@@ -1,34 +1,23 @@
-// Importeer component-specifieke CSS
-import '../../css/components/profile.css';
-import '../../css/components/course-card.css';
-import '../../css/components/progress.css';
-import '../../css/components/newsletter.css';
-import '../../css/components/form.css';
-import '../../css/components/alert.css';
-import '../../css/components/tools.css';
-import '../../css/components/news.css';
+// Importeer component-specifieke CSS (georganiseerd alfabetisch, geen duplicaten)
 import '../../css/components/about.css';
 import '../../css/components/account.css';
+import '../../css/components/alert.css';
 import '../../css/components/auth.css';
 import '../../css/components/cart.css';
-
-// Import all CSS components
+import '../../css/components/course-card.css';
+import '../../css/components/cta.css';
+import '../../css/components/footer.css';
+import '../../css/components/form.css';
+import '../../css/components/hero.css';
 import '../../css/components/navbar.css';
 import '../../css/components/navbar-fix.css'; // Fix for !important issues
-import '../../css/components/footer.css';
-import '../../css/components/hero.css';
-import '../../css/components/testimonials.css';
-import '../../css/components/slider.css';
-import '../../css/components/cta.css';
-import '../../css/components/cart.css';
-import '../../css/components/profile.css';
-import '../../css/components/course-card.css';
-import '../../css/components/progress.css';
+import '../../css/components/news.css';
 import '../../css/components/newsletter.css';
-import '../../css/components/form.css';
-import '../../css/components/alert.css';
-import '../../css/components/account.css';
-import '../../css/components/auth.css';
+import '../../css/components/profile.css';
+import '../../css/components/progress.css';
+import '../../css/components/slider.css';
+import '../../css/components/testimonials.css';
+import '../../css/components/tools.css';
 
 // Eventueel aanvullende component logica kan hier later worden toegevoegd. 
 
